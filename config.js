7481aebe12fb98226969e48a3358fda4e761bc62
triggerderler <avaburak17@gmail.com>
2024-05-14 15:39:40 +0000

    Update

--- config.js
+++ config.js
@@ -1,24 +1,21 @@
 const config = {
-    "token": "TOKEN",
-    "guild": "SERVER ID",
-    "HUBtxtChannelID": "leaveBlank",
-    "HUBvcChannelID": "HUB CHANNEL ID | VC hub channel u need create by u owne, and put the hub id in here.",
-    "DefaultRoleID": "if use @everyone, just put the server id in here. ",
-    "categoryID": " a category to Collect your voice channels, u need create by you self, adn put category id here.",
-    "status": "",
-    "enable_slash": true,
-    "prefix": "]",
-    "owners": [
-        ""
-    ]
-}
+  HUBtxtChannelID: "leaveBlank",
+  HUBvcChannelID: "1239725139914919956",
+  DefaultRoleID: "1237843005407232091",
+  categoryID: "1239725051947909170",
+  status: "idle",
+  enable_slash: true,
+  prefix: ".",
+  owners: ["440222721079508993"],
+};
 //use env
-config.token = process.env.TOKEN || ''
-config.guild = process.env.SERVER_ID || ''
-config.HUBvcChannelID = process.env.HUB_ID || ''
-config.DefaultRoleID = process.env.ROLE_ID || ''
-config.categoryID = process.env.CATEGORY_ID || ''
-config.prefix = process.env.PREFIX || ']'
-config.owners = process.env.OWNERS ? process.env.OWNERS.split(',') : null || ['']
 
-module.exports = { config }
+config.HUBvcChannelID = process.env.HUB_ID || "";
+config.DefaultRoleID = process.env.ROLE_ID || "";
+config.categoryID = process.env.CATEGORY_ID || "";
+config.prefix = process.env.PREFIX || "]";
+config.owners = process.env.OWNERS
+  ? process.env.OWNERS.split(",")
+  : null || [""];
+
+module.exports = { config };
---
+++ keep_alive.js
@@ -0,0 +1,7 @@
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    res.write("I'm alive");
+    res.end();
+  })
+  .listen(3000);
